66ec984a189d9e3ab76c0dce1594ae2baef940f5
ARUN RAJ <s.arun.raj@gmail.com>
2019-12-29 12:14:28 -0500

    Update User details

---
+++ update_user.py
@@ -0,0 +1,24 @@
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+## REST API End Point URL. Make sure to give the correct server name
+## User Id of the user from Oracle Cloud
+url = "https://<servername>.fa.us2.oraclecloud.com/hcmRestApi/scim/Users/94AE8D4EA2E5A471E0502E0A86CD1E6D"
+
+## Payload. We are updating the Email ID
+payload = """{
+			"schemas":["urn:scim:schemas:core:2.0:User"],
+			"active":true,
+			"emails":[{"primary":true,"value":"arunraj@quest4apps.com","type":"W"}]
+            }"""
+
+## Request Header
+headers = {'Content-Type': "application/json",}
+
+## Invoke the REST API. We are using PATCH method
+## Make sure to provide the correct user name and password.
+response = requests.request("PATCH", url, data=payload, headers=headers,auth=HTTPBasicAuth('araj', 'Itsapassword'))
+
+## Print the response
+print(response.text)
